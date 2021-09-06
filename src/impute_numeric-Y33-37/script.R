b0c087480ce6a92ab070af67d2c5bad9e1fd0dc6
Adam Howes <adamhowes@outlook.com>
2021-09-06 16:52:40 +0100

    Start working on #004

---
+++ src/impute_numeric-Y33-37/script.R
@@ -0,0 +1,18 @@
+# orderly::orderly_develop_start("impute_numeric-Y33-37")
+# setwd("src/impute_numeric-Y33-37")
+
+usa_data <- read_excel("depends/usa_data_july2021.xlsx")
+
+fit_impute <- inla(
+  formula = y ~ 1,
+  family = "xPoisson",
+  control.predictor = list(link = 1),
+  control.compute = list(config = TRUE),
+  data = list(y = usa_data$malrat30)
+)
+
+lambda <- mean(fit_impute$summary.fitted$mean)
+samples <- rpois(1000, lambda) %>%
+  as.data.frame()
+
+#' TODO: Finish this. Current approach seems wrong
